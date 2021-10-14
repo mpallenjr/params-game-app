a5445ad1724b2968ab905307b4e37c9e66e8f8c5
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-13 20:58:27 -0500

    add number guessing feature

--- app/controllers/param_controller.rb
+++ app/controllers/param_controller.rb
@@ -5,15 +5,28 @@ class ParamController < ApplicationController
     p input
     
     if input[0] == "A"
-    message = "your name starts with a"
+      message = "your name starts with a"
     else
-    message = "your name is okay"
+      message = "your name is okay"
     end
     
     render json: {message:input, print:message}
 
   end
 
+  def guess
+    guess = params["number"].to_i
+    correct_number = 30
+
+    if guess > correct_number
+      message = "too high"
+    elsif guess < correct_number
+      message = "too low"
+    else 
+      message = "correct!"
+    end
+    render json: {pring: message}
+  end
 
 end
 
